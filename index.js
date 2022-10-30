a03cc44504c4c09a9234ad7dba8e48025074c463
sami <zaman.sami8@gmail.com>
2022-10-30 21:03:26 +0600

    making input case insensitive

--- index.js
+++ index.js
@@ -17,15 +17,18 @@ while (keepGoing) {
     getPlayerChoice();
     getComputerChoice();
 
+    playerRPS = playerRPS.toUpperCase();
+    randomRPS = randomRPS.toUpperCase();
+
     winCondition = (playerRPS == 'rock' && randomRPS == 'scissor') || (playerRPS == 'paper' && randomRPS == 'rock') || (playerRPS == 'scissor' && randomRPS == 'paper');
 
-    if (playerRPS == 'q') {
+    if (playerRPS == 'Q') {
         keepGoing = false;
     } else if (playerRPS == randomRPS) {
         alert('Draw');
     } else if (winCondition) {
         alert('WIN!')
     } else {
-        alert('you lose')
+        alert('you lose');
     }
 }

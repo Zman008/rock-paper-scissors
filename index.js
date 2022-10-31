03dc2cb23db711a0bfa18027ca35282b40cb0c2b
sami <zaman.sami8@gmail.com>
2022-10-31 09:50:54 +0600

    adding scoring system

--- index.js
+++ index.js
@@ -2,38 +2,62 @@
 
 RPSList = ['rock', 'paper', 'scissor'];
 
+let playerScore = 0;
+let computerScore = 0;
+let displayScore = `Player Score: ${playerScore} \t\t\t\t Computer Score: ${computerScore}`;
+
+
 function getPlayerChoice() {
-    return playerRPS = prompt("rock or paper or scissor \ntype and enter 'q' to exit");
+    playerRPS = prompt("rock or paper or scissor");
+    return playerRPS = playerRPS.toUpperCase();
 }
 
 function getComputerChoice() {
     randomNum = Math.floor(Math.random() * 3);
-    return randomRPS = RPSList[randomNum];
+    randomRPS = RPSList[randomNum];
+    return randomRPS = randomRPS.toUpperCase();
 }
 
 function playRound(playerRPS = getPlayerChoice(), randomRPS = getComputerChoice()) {
     
-    playerRPS = playerRPS.toUpperCase();
-    randomRPS = randomRPS.toUpperCase();
 
     winCondition = (playerRPS == 'ROCK' && randomRPS == 'SCISSOR') || 
         (playerRPS == 'PAPER' && randomRPS == 'ROCK') || 
         (playerRPS == 'SCISSOR' && randomRPS == 'PAPER');
 
-    if (playerRPS == 'Q') {
-        keepGoing = false;
-    } else if (playerRPS == randomRPS) {
-        alert('Draw');
+    if (playerRPS == randomRPS) {
+        displayScore = `Player Score: ${playerScore} \t\t\t\t Computer Score: ${computerScore}`;
+        alert('Draw \n\n' + displayScore);
     } else if (winCondition) {
-        alert('WIN!')
+        playerScore++;
+        displayScore = `Player Score: ${playerScore} \t\t\t\t Computer Score: ${computerScore}`;
+
+        alert('WIN! \n\n' + displayScore);
     } else {
-        alert('you lose');
+        computerScore++;
+        displayScore = `Player Score: ${playerScore} \t\t\t\t Computer Score: ${computerScore}`;
+        
+        alert('you lose \n\n' + displayScore);
     }
+    
+    displayScore = `Player Score: ${playerScore} \t\t\t\t Computer Score: ${computerScore}`;
+    
 }
 
+let keepGoing = true;
+
 function game() {
-    for (i=0; i<5; i++){
+    while (keepGoing) {
+        
         playRound();
+
+        if (playerScore == 3) {
+            alert('GAME OVER \n You Win\n\n' + displayScore);
+            keepGoing = false;
+        } else if (computerScore == 3) {
+            alert('GAME OVER \n You Lose\n\n' + displayScore);
+            keepGoing = false;
+        }
     }
 }
 

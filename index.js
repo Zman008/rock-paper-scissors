7609551044f7b3d91d83b0e59f77afa4e8ace756
Zaman Sami <zaman.sami8@gmail.com>
2023-05-21 11:56:35 +0600

    adding images to the game

--- index.js
+++ index.js
@@ -10,41 +10,54 @@ const html = document.querySelector("html");
 const result = document.querySelector(".gameResult");
 const computerScoreDisplay = document.querySelector('.computer-score');
 const playerScoreDisplay = document.querySelector('.player-score');
-
 const btns = document.querySelectorAll("button");
+const computerChoiceImg = document.querySelector(".cmp-choice-img");
+const playerChoiceImg = document.querySelector(".plyr-choice-img");
 
 btns.forEach((btn) => {
   btn.addEventListener("click", function () {
     playerRPS = btn.id;
+    playerChoiceImg.src = `./img/${playerRPS}.png`;
+    
     playRound();
+
+    if (playerScore == 5) {
+      alert("You win!");
+      playerScore = 0;
+      computerScore = 0;
+    } else if (computerScore == 5) {
+      alert("You lose!");
+      playerScore = 0;
+      computerScore = 0;
+    }
   });
 });
 
 function getComputerChoice() {
   randomNum = Math.floor(Math.random() * 3);
-  return (randomRPS = RPSList[randomNum]);
+  return RPSList[randomNum];
 }
 
 function playRound(randomRPS = getComputerChoice()) {
+  computerChoiceImg.src = `./img/${randomRPS}.png`;
   winCondition =
     (playerRPS == "ROCK" && randomRPS == "SCISSORS") ||
     (playerRPS == "PAPER" && randomRPS == "ROCK") ||
     (playerRPS == "SCISSORS" && randomRPS == "PAPER");
 
   if (playerRPS == randomRPS) {
-    result.textContent = 'Draw';
+    result.src = './img/equal.png';
     html.style.backgroundColor = 'grey';
   } else if (winCondition) {
     playerScore++;
-    result.textContent = 'Win';
-    html.style.backgroundColor = 'LimeGreen';
+    result.src = './img/circle.png';
+    html.style.backgroundColor = 'palegreen';
   } else {
     computerScore++;
-    result.textContent = 'Lose';
-    html.style.backgroundColor = 'OrangeRed';
+    result.src = './img/cross.png';
+    html.style.backgroundColor = 'tomato';
   }
   
   computerScoreDisplay.textContent = computerScore;
   playerScoreDisplay.textContent = playerScore;
 }
-
